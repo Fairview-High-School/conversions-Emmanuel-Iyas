8b664beef1873c95027d0664506ffac59453a59a
Emmanuel-Iyas <eoiyasele02@bvsd.org>
2025-08-20 18:57:50 +0000

    Added Menu

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,15 @@
+// Emmanuel I
+// 08/20/25
 
+public class Conversions
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("4. Feet to meters");
+        System.out.println("5. Meters to Feet");
+        System.out.println("6. Ounces to Milliliters");
+        System.out.println("7. Milliliters to ounces");
+    }
+}
